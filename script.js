54fd1588e97291f4f6f86a226a9090cbd174a875
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2025-03-11 15:25:13 +0400

    rrrr

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ let allData = [];
 
 fetch("data.json")
   .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
     return response.json();
   })
   .then((data) => {
